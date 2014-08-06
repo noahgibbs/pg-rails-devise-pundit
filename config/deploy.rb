0cba2c626f0c969d7e3fcb7486b76d6b12d6de6e
Noah Gibbs <noah_gibbs@yahoo.com>
2014-08-05 21:38:26 -0700

    Switch to repo that can be checked out without an existing GitHub.com SSH key

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'rails-devise-pundit'
-set :repo_url, 'git@github.com:noahgibbs/rails-devise-pundit.git'
+set :repo_url, 'https://github.com/noahgibbs/rails-devise-pundit.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
