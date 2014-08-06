3eb327758946c7aadefd447c87d6965673f01b3c
Noah Gibbs <noah_gibbs@yahoo.com>
2014-08-05 20:57:41 -0700

    Set app name and repo correctly

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'rails-devise-pundit'
+set :repo_url, 'git@github.com:noahgibbs/rails-devise-pundit.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
